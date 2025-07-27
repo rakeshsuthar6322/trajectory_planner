e8551abf327c828c0d4cfa99e6adda2ffb50a690
rak6827s <Rakesh.Suthar@stud.hs-coburg.de>
2025-07-27 18:42:53 +0200

    Finalize overtaking logic and code cleanup

--- build/tp_package/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/autonomous/tp_ws/src/TheElite_trajectory_planner/install/tp_package'
